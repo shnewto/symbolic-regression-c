8b16a9af8d098865c4860ca042824f46fab6ffa7
Shea Newton <shnewto@gmail.com>
2016-05-09 14:02:47 -0700

    Revised population test after recent global memory reworking.

--- test/test_population.c
+++ test/test_population.c
@@ -3,6 +3,7 @@
 #include <cgreen/cgreen.h>
 
 #include "population.h"
+#include "global_memory.h"
 
 population_s *test_population;
 
@@ -10,29 +11,29 @@ Describe(population_tests);
 
 BeforeEach(population_tests)
 {
-    test_population = population_alloc();
+    init_global_memory();
 }
 
 AfterEach(population_tests)
 {
-    population_free( &test_population );
+    release_global_memory();
 }
 
 Ensure( population_tests, alloc_and_free_succeed )
 {
 
     population_s *population = NULL;
-    
+
     population = population_alloc();
 
     assert_that(
             population,
             is_not_equal_to(NULL) );
-    
+
     population_free( &population );
-    
+
     assert_that(
-            population, 
+            population,
             is_equal_to(NULL));
 }
 
